43cb5c77c11a7a27cd30411709aee1dd1168dd87
David Pacheco <david.pacheco@beubi.com>
2016-01-12 17:57:08 +0000

    Update AP mode script

--- src/filesystem/home/pi/wifi_ap_mode.sh
+++ src/filesystem/home/pi/wifi_ap_mode.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
+  
+sudo rm /etc/network/interfaces
+sudo cp /etc/network/interfaces_ap.dist /etc/network/interfaces
 
-if [ ! -f /etc/wpa_supplicant/wpa_supplicant.conf ]; then
-    
-    sudo rm /etc/network/interfaces
-    sudo cp /etc/network/interfaces_ap.dist /etc/network/interfaces
+sudo ifdown wlan0
 
-    sudo ifdown wlan0
+sudo service networking restart
+sudo service dnsmasq start
+sudo service hostapd start
 
-    sudo service networking restart
-    sudo service dnsmasq start
-    sudo service hostapd start
-fi
