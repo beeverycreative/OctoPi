7a4b68a801a0d7adad4d019d346a2230c8256c55
João Grego <jpgrego@gmail.com>
2016-04-15 16:40:26 +0100

    Some bugs fixed and inefficiencies resolved
    - Modified the AP and client scripts to be more robust
    - Added /etc/default/keyboard to automatically set keyboard layout to PT
      (for testing purposes only?)
    - Modified /etc/init.d/wifi_ap_mode to run the client script in case
      /etc/wpa_supplicant/wpa_supplicant.conf file exists

--- src/filesystem/home/pi/wifi_ap_mode.sh
+++ src/filesystem/home/pi/wifi_ap_mode.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 
-LSUSB_RESULT=`lsusb`
-WIFI_PEN_IDS=("7392:7811" "0bda:8176")
-PEN_FOUND=false
-
+# kill any of these, if running
+sudo pkill hostapd
 sudo pkill dhcpcd
+sudo pkill wpa_supplicant
 sudo systemctl stop dnsmasq
 
-sudo ip addr flush dev wlan0
+# reset wlan0 interface, and add IP manually, for AP mode
 sudo ip link set wlan0 down
+sudo ip addr flush dev wlan0
 sudo ip addr add 10.0.0.1/24 dev wlan0
 sudo ip link set wlan0 up
 
 sudo systemctl start dnsmasq
 
-sleep 3
-
-for id in ${WIFI_PEN_IDS[@]}; do
-    if [[ `echo $LSUSB_RESULT | grep $id` ]]; then
-        PEN_FOUND=true
-        sudo hostapd /etc/hostapd/hostapd.conf.external &
-        break
-    fi
-done
-
-if [ $PEN_FOUND = false ]; then
-    sudo hostapd /etc/hostapd/hostapd.conf.internal &
+# search for any external USB pen
+# it's a stupid method of determining if we should use
+# the config file for external pen drives, on rpi2
+# if you use an external wifi usb pen on rpi3, it will try
+# to use the config file on wlan0 anyway, which is usually
+# the internal adapter
+if [[ `lsusb -v | grep WLAN` ]]; then
+    sudo hostapd -B /etc/hostapd/hostapd.conf.external
+else    
+    sudo hostapd -B /etc/hostapd/hostapd.conf.internal
 fi
 
--- src/filesystem/home/pi/wifi_client_mode.sh
+++ src/filesystem/home/pi/wifi_client_mode.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
+# kill any of these, if running
 sudo pkill hostapd
 sudo pkill dhcpcd
+sudo pkill wpa_supplicant
+sudo systemctl stop dnsmasq
 
+# reset wlan0 interface
 sudo ip link set wlan0 down
 sudo ip addr flush dev wlan0
 sudo ip link set wlan0 up
 
-sudo wpa_supplicant -D nl80211,wext -c /etc/wpa_supplicant/wpa_supplicant.conf -i wlan0
-sudo dhcpcd wlan0 &
+# copy the generated wpa_supplicant.conf to it's proper place and run 
+# wpa_supplicant afterwards
+sudo cp $HOME/wpa_supplicant_update.conf /etc/wpa_supplicant/wpa_supplicant.conf
+sudo wpa_supplicant -B -D nl80211,wext -c /etc/wpa_supplicant/wpa_supplicant.conf -i wlan0
+
+# wait for wpa_supplicant to associate to AP successfully
+# couldn't find a better way to do this, like the handy -w option in dhcpcd
+while [[ -z `iwconfig wlan0 | grep ESSID` ]]; do
+    echo "waiting for AP to associate..."
+    sleep 1
+done
+
+# finally, acquire IP. fork when successfully acquired
+sudo dhcpcd -w wlan0
 
