908107b607bccf2c938b77a0dec61bab5305ab6e
João Grego <jpgrego@gmail.com>
2016-04-22 18:27:00 +0100

    Tweaks and kivy installation
    - improvements on wifi scripts (client and AP mode)
    - display is now activated by default
    - kivy is installed

--- src/filesystem/home/pi/wifi_ap_mode.sh
+++ src/filesystem/home/pi/wifi_ap_mode.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
 # kill any of these, if running
+echo "Killing hostapd..."
 sudo pkill hostapd
+echo "Killing dhcpcd..."
 sudo pkill dhcpcd
+echo "Killing wpa_supplicant..."
 sudo pkill wpa_supplicant
+echo "Stopping dnsmasq..."
 sudo systemctl stop dnsmasq
 
 # reset wlan0 interface, and add IP manually, for AP mode
+echo "Resetting wlan0..."
 sudo ip link set wlan0 down
 sudo ip addr flush dev wlan0
+echo "Setting AP IP 10.0.0.1 to wlan0"
 sudo ip addr add 10.0.0.1/24 dev wlan0
+echo "Bringing wlan0 back up"
 sudo ip link set wlan0 up
 
+echo "Starting dnsmasq"
 sudo systemctl start dnsmasq
 
 # search for any external USB pen
@@ -20,6 +28,7 @@ sudo systemctl start dnsmasq
 # if you use an external wifi usb pen on rpi3, it will try
 # to use the config file on wlan0 anyway, which is usually
 # the internal adapter
+echo "Starting hostapd"
 if [[ `lsusb -v | grep WLAN` ]]; then
     sudo hostapd -B /etc/hostapd/hostapd.conf.external
 else    
--- src/filesystem/home/pi/wifi_client_mode.sh
+++ src/filesystem/home/pi/wifi_client_mode.sh
@@ -1,28 +1,51 @@
 #!/bin/bash
 
+MODULE=`readlink /sys/class/net/wlan0/device/driver/module | grep -o '[^/]*$'`
+
 # kill any of these, if running
+echo "Killing hostapd..."
 sudo pkill hostapd
+
+echo "Killing dhcpcd..."
 sudo pkill dhcpcd
+
+echo "Killing wpa_supplicant..."
 sudo pkill wpa_supplicant
+
+if [[ -f /var/run/wpa_supplicant/wlan0 ]]; then
+	sudo rm /var/run/wpa_supplicant/wlan0
+fi
+
+echo "Stopping dnsmasq..."
 sudo systemctl stop dnsmasq
 
+# this helps in transitioning from AP to client, seems to fail way less times
+echo "Removing and inserting $MODULE"
+sudo modprobe -r $MODULE
+sudo modprobe $MODULE
+sleep 3
+
 # reset wlan0 interface
+echo "Resetting wlan0 interface..."
 sudo ip link set wlan0 down
 sudo ip addr flush dev wlan0
 sudo ip link set wlan0 up
 
 # copy the generated wpa_supplicant.conf to it's proper place and run 
 # wpa_supplicant afterwards
-sudo cp $HOME/wpa_supplicant_update.conf /etc/wpa_supplicant/wpa_supplicant.conf
+echo "Copying wpa_supplicant_update.conf..."
+sudo cp -v $HOME/wpa_supplicant_update.conf /etc/wpa_supplicant/wpa_supplicant.conf
+echo "Starting wpa_supplicant..."
 sudo wpa_supplicant -B -D nl80211,wext -c /etc/wpa_supplicant/wpa_supplicant.conf -i wlan0
 
 # wait for wpa_supplicant to associate to AP successfully
 # couldn't find a better way to do this, like the handy -w option in dhcpcd
-while [[ -z `iwconfig wlan0 | grep ESSID` ]]; do
-    echo "waiting for AP to associate..."
+while [[ -z `iwgetid wlan0 -r` ]]; do
+    echo "Waiting for AP to associate..."
     sleep 1
 done
 
 # finally, acquire IP. fork when successfully acquired
-sudo dhcpcd -w wlan0
+echo "Acquiring IP..."
+sudo dhcpcd -w wlan0 -t 0
 
