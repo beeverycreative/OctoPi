b6bb25575ca668ed7bcb7261b72c3b3d09508da2
dpacheco <david.pacheco@beubi.com>
2016-06-30 15:55:04 +0100

    Vagrant box setup

--- src/vagrant/setup.sh
+++ src/vagrant/setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+
 sudo apt-get update
 sudo apt-get install -y gawk util-linux realpath git qemu-user-static unzip zip
 
-cd /BEEwebPi/src
+cd /home/vagrant/BEEwebPi/src
 sudo bash ./build
